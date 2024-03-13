50f6b0d029850cfe4839547ddaf5b1524acca9eb
imsuneth <imsuneth@gmail.com>
2024-03-14 03:56:03 +1100

    fix test not passing on ubuntu 22

--- test/test.sh
+++ test/test.sh
@@ -30,9 +30,10 @@ echo_bams() {
 mkdir -p temp || die "Failed to create temp directory"
 
 echo "Test 1"
-echo_bams | ex ./realfreq -o temp/example-ont-real.tsv || die "Test 1 failed running realfreq"
-cat temp/example-ont-real.tsv | sort -n -k2,2 -k10,10 > temp/example-ont-real-s.tsv
-diff -q test/example-ont-freq-s.tsv temp/example-ont-real-s.tsv || die "Test 1 diff failed"
+echo_bams | ex ./realfreq -o temp/example-ont-freq.tsv || die "Test 1 failed running realfreq"
+cat temp/example-ont-freq.tsv | sort -n -k2,2 -k10,10 > temp/example-ont-freq-actual.tsv
+cat test/example-ont-freq.tsv | tail -n +2 - | sort -n -k2,2 -k10,10 > temp/example-ont-freq-expected.tsv
+diff -q temp/example-ont-freq-expected.tsv temp/example-ont-freq-actual.tsv || die "Test 1 failed"
 
 
 echo "TESTS PASSED!"
